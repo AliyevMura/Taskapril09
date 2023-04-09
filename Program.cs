04fdc87305e02f2d4734f6047da4cdd0f2af9054
AliyevMura <7lvghl9@code.edu.az>
2023-04-09 14:28:32 +0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿
+using System;
+using System.Net.Cache;
+using System.Text;
+
+namespace Task09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] students = new Student[]
+            {
+                new Student("Ali", "Seferli", "AB201", 15, false),
+                new Student("Muslum", "Misirli", "AB201", 75, false),
+                new Student("Murad", "Aliyev", "AB201", 90, true),
+                new Student("Malak", "Mansimova", "AB201", 60, false),
+                new Student("Nigar", "Mammadova", "AB201", 95, true)
+            };
+
+            ShowInfo(students);
+            Console.ReadLine();
+        }
+
+        public static void ShowInfo(Student[] students)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var student in students)
+            {
+                sb.AppendLine(student.StudentInfo());
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Group { get; set; }
+        public int Point { get; set; }
+        public bool IsGraduated { get; set; }
+
+        public Student(string name, string surname, string group, int point, bool isGraduated)
+        {
+            Name = name;
+            Surname = surname;
+            Group = group;
+            Point = point;
+            IsGraduated = isGraduated;
+
+
+        }
+
+        public string FullName()
+        {
+            return $"{Name} {Surname}";
+        }
+
+        public bool Exam()
+        {
+            if (Point > 80)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public string StudentInfo()
+        {
+            string graduationstatus = IsGraduated ? "Mezun oldu" : "Mezun olmadi";
+            return $"Name: {Name}, Surname: {Surname}, Group: {Group}, Point: {Point}, {graduationstatus}";
+        }
+    }
+}
+
+
+
---
+++ class/Student.cs
@@ -0,0 +1,72 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Task09
+//{
+//    class Student
+//    {
+//        public Student()
+//        {
+//            Console.WriteLine("murad");
+//        }
+
+//        public Student(string name)
+//        {
+//            this.name = name;
+//        }
+
+
+//        public Student(string name, string surname) : this(name)
+//        {
+
+//            this.surname = surname;
+//        }
+
+
+//        public Student(string name, string surname, int age) : this(name, surname)
+//        {
+
+//            this.age = age;
+//        }
+
+//        public Student(string name, string surname, int age, string Group) : this(name, surname, age)
+//        {
+
+//            this.Group = Group;
+//        }
+
+
+//        public Student(string name, string surname, int age, string Group, int point) : this(name, surname, age, Group)
+//        {
+
+//            this.point = point;
+//        }
+
+
+
+//        public Student(string name, string surname, int age, string Group, int point, bool IsGraudated) : this(name, surname, age, Group, point)
+//        {
+
+//            this.IsGraudated = IsGraudated;
+//        }
+
+//        public string name;
+//        public string surname;
+//        public int age;
+//        public string Group;
+//        public int point;
+//        public bool IsGraudated;
+
+
+//        public void ShowInfo()
+//        {
+//            Console.WriteLine($"{name}  {surname} {age} {Group} {point} {IsGraudated}");
+//        }
+//    }
+
+
+//}
+
